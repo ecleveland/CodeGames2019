df2734114ba747fd274a6600fd5f82c82ea5d567
Eric Cleveland <eric@clevelandcloud.com>
2019-05-15 17:26:02 -0500

    This is Chris Goldkamp's Fault*

---
+++ CodeGames2019_UI/CodeGames2019_UI/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeGames2019_UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
