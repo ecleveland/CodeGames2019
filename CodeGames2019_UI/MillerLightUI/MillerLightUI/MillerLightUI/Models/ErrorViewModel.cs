17d350af35cc13ab68c0e7d0f714806e8d127888
Eric Cleveland <eric@clevelandcloud.com>
2019-05-15 20:53:57 -0500

    Hwy... Use this one...

---
+++ CodeGames2019_UI/MillerLightUI/MillerLightUI/MillerLightUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MillerLightUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
